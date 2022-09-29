7bad989beae4663bfd33fe8d408f629484a487de
Salma <salma.afifi5555@gmail.com>
2022-09-30 00:41:46 +0200

    project structure set up

---
+++ src/main/java/com/thesis/fixable/FixableApplication.java
@@ -0,0 +1,13 @@
+package com.thesis.fixable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixableApplication.class, args);
+	}
+
+}
